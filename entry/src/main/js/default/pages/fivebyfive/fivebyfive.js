51627f7cc1e3c41e660d50601fed2e298d63aad1
richardyyao <97313676+richardyyao@users.noreply.github.com>
2022-09-30 13:01:44 -0700

    Update fivebyfive.js

--- entry/src/main/js/default/pages/fivebyfive/fivebyfive.js
+++ entry/src/main/js/default/pages/fivebyfive/fivebyfive.js
@@ -193,6 +193,8 @@ export default {
         this.onShow();
     },
     checkBack() {
-        router.back();
-    },
+        router.replace({
+            uri: 'pages/home/home'
+        });
+    }
 }

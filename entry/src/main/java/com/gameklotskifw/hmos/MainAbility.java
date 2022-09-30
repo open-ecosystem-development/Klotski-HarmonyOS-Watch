f2e5e8f86adbb7660b2c60eb6e067f1bdf59d744
richardyyao <97313676+richardyyao@users.noreply.github.com>
2022-09-30 12:56:05 -0700

    Update MainAbility.java

--- entry/src/main/java/com/gameklotskifw/hmos/MainAbility.java
+++ entry/src/main/java/com/gameklotskifw/hmos/MainAbility.java
@@ -15,11 +15,17 @@ package com.gameklotskifw.hmos;
 
 import ohos.ace.ability.AceAbility;
 import ohos.aafwk.content.Intent;
+import ohos.multimodalinput.event.TouchEvent;
 
 public class MainAbility extends AceAbility {
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
+    }
+    @Override
+    public boolean onTouchEvent(TouchEvent event) {
+        super.onTouchEvent(event);
+        return true;
     }
     @Override
     public void onStop() {
